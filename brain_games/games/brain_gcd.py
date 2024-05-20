f4e428d50e91640e95bf4367412222524e69da4f
aphmelev <i@aphmelev.ru>
2024-05-20 22:34:40 +0700

    preprefinal

---
+++ brain_games/games/brain_gcd.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import random
+import prompt
+
+
+def gcd_fun(x, y):
+    if y == 0:
+        return x
+    else:
+        return gcd_fun(y, x % y)
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    print('Find the greatest common divisor of given numbers.')
+    count_answer = 0
+    beg = 1
+    end = 100
+
+    for _ in range(3):
+        random_int_one = random.randint(beg, end)
+        random_int_two = random.randint(beg, end)
+        print(f'Question: {random_int_one} {random_int_two}')
+        num = gcd_fun(random_int_one, random_int_two)
+        answer = prompt.string('Your answer: ')
+
+        if answer == str(num):
+            print('Correct!')
+            count_answer += 1
+        else:
+            print(f"'{answer}' is wrong answer;(.Correct answer was '{num}'.")
+            print(f"Let's try again, {name}!")
+            break
+    if count_answer == 3:
+        print(f'Congratulations, {name}!')
---
+++ brain_games/games/brain_prime.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+import random
+import prompt
+
+
+def is_simple(a):
+    k = 0
+    for i in range(2, a // 2 + 1):
+        if a % i == 0:
+            k = k + 1
+    if k <= 0:
+        return True
+    else:
+        return False
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    print('Answer "yes" if given number is prime. Otherwise answer "no".')
+    count_answer = 0
+
+    for _ in range(3):
+        rand_num = random.randint(1, 100)
+        print(f'Question: {rand_num}')
+        simple = is_simple(rand_num)
+        answer = prompt.string('Your answer: ')
+
+        if answer == 'yes' or not simple:
+            opposite_answer = 'no'
+        else:
+            opposite_answer = 'yes'
+
+        if answer.lower() == 'yes' and simple:
+            print('Correct!')
+            count_answer += 1
+        elif answer.lower() == 'no' and not simple:
+            print('Correct!')
+            count_answer += 1
+        else:
+            print(f"'{answer}' is wrong answer;(.Correct answer was '{opposite_answer}'.")
+            print(f"Let's try again, {name}!")
+            break
+
+    if count_answer == 3:
+        print(f'Congratulations, {name}!')
